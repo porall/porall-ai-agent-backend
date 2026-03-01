afade4f721b11ba2f4346235b9066cd5a2088dc7
porall <625513044@qq.com>
2026-03-01 22:13:37 +0800

    feat: 初始化项目

---
+++ src/main/java/com/porall/poaiagent/PorallAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.porall.poaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorallAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PorallAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/porall/poaiagent/PorallAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porall.poaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorallAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
