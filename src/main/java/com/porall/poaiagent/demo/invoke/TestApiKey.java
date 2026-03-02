75d91609e540cd306b583ecdb0c3ce20c4c56efd
porall <625513044@qq.com>
2026-03-02 21:20:22 +0800

    feat: 使用sdk的方式调用ai大模型

---
+++ src/main/java/com/porall/poaiagent/demo/invoke/TestApiKey.java
@@ -0,0 +1,10 @@
+package com.porall.poaiagent.demo.invoke;
+
+/**
+ * 仅用于测试
+ */
+public interface TestApiKey {
+    //    String API_KEY = "需要申请API_KEY";
+        String API_KEY = "sk-3662f316061541cda6f18767dca8d95a";
+
+}
